d007a96377621a022bb132afc3a703ddbd7f1099
wlshen@umich.edu <wlshen@umich.edu>
2019-08-09 17:03:22 -0400

    Updates

---
+++ precache-manifest.7e3d8d522af811762351dda1d585fc05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75b0405a21d410b7ec560041911fea5",
+    "url": "/wlshen123/react-gh-pages/index.html"
+  },
+  {
+    "revision": "d2309fdcbf98f9a4b9c9",
+    "url": "/wlshen123/react-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/wlshen123/react-gh-pages/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "d2309fdcbf98f9a4b9c9",
+    "url": "/wlshen123/react-gh-pages/static/js/main.751dc1ac.chunk.js"
+  },
+  {
+    "revision": "b67620633ffd85bcd01d",
+    "url": "/wlshen123/react-gh-pages/static/js/runtime~main.c8d09b34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wlshen123/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
